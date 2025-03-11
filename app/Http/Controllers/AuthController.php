a499878010fb2089642b22485bec7e730471cd70
Autumn Charles <autumn.m.s.c@gmail.com>
2025-03-10 22:22:57 -0400

    completed exercise 4

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    //the two methods
+	public function create(){
+		//get the form for login
+		return view('login-form');
+	}
+	public function store(Request $request){
+		//get the user info to check if correct or not
+		$credentials = $request->validate([
+			'email' => ['required', 'email'],
+			'password' => ['required'],
+		]);
+		if(Auth::attempt($credentials)){
+			$request->session()->regenerate();
+			
+			return redirect('dashboard');
+		}
+		
+		return back()->withErrors([
+			'email' => 'Invalid credentials. Check the email address and password entered.',])->onlyInput('email');
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
 
 // Create additional Routes below
+
+Route::get('/login', [AuthController::class, 'create'])->name('login');
+
+Route::post('/login', [AuthController::class, 'store']);
